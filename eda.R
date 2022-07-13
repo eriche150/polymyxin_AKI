c7f9cbf96acb51761427c70a45e1d646ff880d25
ehe <erhe@health.ucsd.edu>
2022-07-13 17:17:22 -0400

    added subgroup analysis to original file

--- eda.R
+++ eda.R
@@ -66,3 +66,27 @@ ggplot(data=prelimdf,aes(x=Time,y=ColBConc,color=factor(`Stage (worst)`)))+
         scale_y_continuous(breaks=c(0,1,3,5),limits=c(0,10))+
         scale_x_continuous(breaks=c(0,1,3,5,6),limits=c(0,10))+
         theme_bw()
+
+
+#####
+#Look at trends between two groups: + AKI and - AKI 
+#Start by subsetting the dataframe into two different df's
+AKI<- prelimdf %>% 
+        filter(prelimdf$AKI=="Y")
+nonAKI<-prelimdf %>% 
+        filter(prelimdf$AKI == "N")
+
+#####df subset#####
+subgroupdf<-prelimdf[!is.na(prelimdf$`Day AKI`),]
+subgroupdf$dDate<-ifelse(subgroupdf$'Day AKI'<= 6, 'A','B') #create new column to separate pt who experienced Day AKI 
+
+#####plot Day AKI on x-axis versus ColA,B, and everything else mentioned, maybe peak Scr as well? 
+ggplot(data=subgroupdf,
+       aes(x=dDate,y=as.numeric(subgroupdf$`Peak Cr`)))+
+        geom_boxplot()+
+        xlab("Date of AKI")+
+        ylab("Peak Scr levels during admission (mg/dL)")+
+        ggtitle("Peak Scr and Date of AKI")+
+        scale_x_discrete(labels=c("AKI achieved < 6 days","AKI achieved >= 6 days"))+
+        scale_y_continuous(breaks=c(0,1,3,5,7,9))+
+        theme_bw()
